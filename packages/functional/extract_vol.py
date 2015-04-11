f855b5d319fc390a0726ce29e5a3e34e9bc631cc
ebridge2 <ebridge2@jhu.edu>
2015-04-11 19:03:00 -0400

    updating with the previous files I had in here...

--- packages/functional/extract_vol.py
+++ packages/functional/extract_vol.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 
 # Copyright 2015 Open Connectome Project (http://openconnecto.me)
 #
@@ -14,9 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-#
-# extract_b0.py
-# Created by Greg Kiar on 2015-02-21.
+
+# extract_vol.py
+# Created by Greg Kiar on 2015-02-21;
+
+# edited by Eric Bridgeford on 2015-04-08.
 # Email: gkiar@jhu.edu
 # Copyright (c) 2015. All rights reserved.
 
@@ -24,37 +26,38 @@ from argparse import ArgumentParser
 from nibabel import load, save, Nifti1Image
 from numpy import where, loadtxt
 
-def extract_vol(dti_img, bvals, b0_vol):
-  print "Loading dti data..."
-  d_img = load(dti_img)
-  b0_data = d_img.get_data()
-  b0_head = d_img.get_header()
+def extract_vol(func_img, vol_val, v0_vol):
+  print "Loading fMRI data..."
+  f_img = load(func_img)
+  v0_data = f_img.get_data()
+  v0_head = f_img.get_header()
   
-  print "Loading bvals file..."
-  b = loadtxt(bvals)
+ # print "Loading vol_val file..."
+ # b = loadtxt(vol_val)
   
-  print "Extracting B0 volume..."
-  b0_data=b0_data[:,:,:,int(where(b==0)[0])]
+  print "Extracting volume..."
+  v0_data=v0_data[:,:,:,vol_val]
   
   print "Updating image header..."
-  b0_head.set_data_shape(b0_head.get_data_shape()[0:3])
+  v0_head.set_data_shape(v0_head.get_data_shape()[0:3])
 
   print "Saving..."
-  out = Nifti1Image( data=b0_data, affine=d_img.get_affine(), header=b0_head )
-  save(out, b0_vol)
+  out = Nifti1Image(v0_data,f_img.get_affine(),v0_head)
+  save(out, v0_vol)
+
   print "Complete!"
 
 
 def main():
   parser = ArgumentParser(description="")
-  parser.add_argument("dti", action="store", help="The DTI image we want to extract B0 from (.nii, .nii.gz)")
-  parser.add_argument("bvals", action="store", help="The b-value file corresponding to the DTI image (.b)")
-  parser.add_argument("b0", action="store", help="The output file location of the B0 scan (.nii, .nii.gz)")
+  parser.add_argument("fmr", action="store", help="The fMRI image we want to extract vol from (.nii, .nii.gz)")
+  parser.add_argument("vol_val", action="store", help="The volume file corresponding to the fmri image (int)")
+  parser.add_argument("v0", action="store", help="The output file location of the B0 scan (.nii, .nii.gz)")
 
   result = parser.parse_args()
 
-  extract_vol(result.dti, result.bvals, result.b0)
-  
+  extract_vol(result.fmr, result.vol_val, result.v0)
+
 if __name__ == "__main__":
     main()
     
